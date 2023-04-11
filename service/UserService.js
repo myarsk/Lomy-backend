44897c47d27d01c770f63c9e4c5f70ef6a42a775
myarsk <109442823+myarsk@users.noreply.github.com>
2023-04-11 07:15:50 -0700

    Update UserService.js

--- service/UserService.js
+++ service/UserService.js
@@ -1,4 +1,3 @@
-
 const UserService = {
   userRepository: null,
   ipLogRepository: null,
@@ -18,76 +17,101 @@ const UserService = {
     this.jwtProvider = jwtProvider;
     this.javaMailSender = javaMailSender;
     this.requestService = requestService;
+    this.createSuperUser();
+  },
 
-    if (roleRepository.findUserRoleByRoleName("ROLE_AUTHOR").isEmpty())
-      roleRepository.save(new UserRole("ROLE_AUTHOR", "ROLE_AUTHOR"));
+  createSuperUser() {
+    this.roleRepository.findUserRoleByRoleName("ROLE_SUPER", (err, role) => {
+      if (err) {
+        // handle error
+      } else if (!role) {
+        this.roleRepository.save(new UserRole("ROLE_SUPER", "ROLE_SUPER"), (err, role) => {
+          if (err) {
+            // handle error
+          } else {
+            this.createSuperUserWithRole(role);
+          }
+        });
+      } else {
+        this.createSuperUserWithRole(role);
+      }
+    });
+  },
 
-    if (roleRepository.findUserRoleByRoleName("ROLE_ADMIN").isEmpty())
-      roleRepository.save(new UserRole("ROLE_ADMIN", "ROLE_ADMIN"));
-
-    if (userRepository.findByUserName("SUPER_MANAGER").isEmpty()) {
-      let roles = new Array();
-      if (roleRepository.findUserRoleByRoleName("ROLE_SUPER").isEmpty())
-        roleRepository.save(new UserRole("ROLE_SUPER", "ROLE_SUPER"));
-      roles.add(roleRepository.findUserRoleByRoleName("ROLE_AUTHOR").get());
-      roles.add(roleRepository.findUserRoleByRoleName("ROLE_ADMIN").get());
-      roles.add(roleRepository.findUserRoleByRoleName("ROLE_SUPER").get());
-      let u = new User("SUPER_MANAGER", "$2a$13$M0v0g9XA7NETbB/I7SodkOcx2mugp4LuoDxMt/ck1b2JxaA8K5ja.", roles, "qqq123@gmail.com");
-      u.setActivated(true);
-      userRepository.save(u);
-    }
+  createSuperUserWithRole(role) {
+    let roles = [];
+    this.roleRepository.findUserRoleByRoleName("ROLE_AUTHOR", (err, authorRole) => {
+      if (err) {
+        // handle error
+      } else {
+        roles.push(authorRole);
+        this.roleRepository.findUserRoleByRoleName("ROLE_ADMIN", (err, adminRole) => {
+          if (err) {
+            // handle error
+          } else {
+            roles.push(adminRole);
+            roles.push(role);
+            let user = new User("SUPER_MANAGER", "$2a$13$M0v0g9XA7NETbB/I7SodkOcx2mugp4LuoDxMt/ck1b2JxaA8K5ja.", roles, "qqq123@gmail.com");
+            user.setActivated(true);
+            this.userRepository.save(user);
+          }
+        });
+      }
+    });
   }
 };
 
-    
-const { userRepository, ipLogRepository, authenticationManager, roleRepository, passwordEncoder, jwtProvider, requestService, javaMailSender } = require('./dependencies');
-
-const UserService = {
-  async sendActivationCode(userName) {
-    const user = await userRepository.findByUserName(userName);
-    if (!user) return false;
-    const userEmail = user.email;
-    const msg = {
-      to: userEmail,
-      subject: 'Lomy Activation Code',
-      text: `your activation code is: ${user.activationCode}`,
-    };
-    await javaMailSender.send(msg);
-    return true;
-  },
-
-  async activateUser(email, activationCode) {
-    const user = await userRepository.findByEmail(email);
-    if (!user) return false;
-    if (user.activationCode === activationCode) {
-      user.activationCode = Math.floor(Math.random() * 8999 + 1000);
-      user.activated = true;
-      await userRepository.save(user);
-      return true;
-    }
-    user.activationCode = Math.floor(Math.random() * 8999 + 1000);
-    const userEmail = user.email;
-    const msg = {
-      to: userEmail,
-      subject: 'Lomy Activation Code',
-      text: `your activation code is: ${user.activationCode}`,
-    };
-    await javaMailSender.send(msg);
-    await userRepository.save(user);
-    return false;
-  },
-
-  async signin(loginDTO, httpServletRequest) {
-    const { username, password } = loginDTO;
-    let token = null;
-    const user = await userRepository.findByUserName(username);
-    console.log(requestService.getClientIP(httpServletRequest));
-    const objects = [];
-    if (user) {
-      if (!user.activated) {
-        objects.push('NOT ACTIVATED');
-        return objects;
-      }
+          async function sendActivationCode(userName) {
+              const user = await userRepository.findByUserName(userName);
+              if (!user)
+                  return false;
+              const userEmail = user.email;
+              const msg = {
+                  to: userEmail,
+                  subject: 'Lomy Activation Code',
+                  text: `your activation code is: ${user.activationCode}`
+              };
+              await javaMailSender.send(msg);
+              return true;
+          }
+          
+          async function activateUser(email, activationCode) {
+              const user = await userRepository.findByEmail(email);
+              if (!user)
+                  return false;
+              else {
+                  if (user.activationCode === activationCode) {
+                      // generate new activation code for next time
+                      user.setActivationCode();
+                      user.setActivated(true);
+                      await userRepository.save(user);
+                      return true;
+                  }
+                  user.setActivationCode();
+                  const userEmail = user.email;
+                  const msg = {
+                      to: userEmail,
+                      subject: 'Lomy Activation Code',
+                      text: `your activation code is: ${user.activationCode}`
+                  };
+                  await javaMailSender.send(msg);
+                  await userRepository.save(user);
+              }
+              return false;
+          }
+          
+          async function signin(loginDTO, httpServletRequest) {
+              const username = loginDTO.username;
+              const password = loginDTO.password;
+              const token = null;
+              const user = await userRepository.findByUserName(username);
+              console.log(requestService.getClientIP(httpServletRequest));
+              const objects = [];
+              if (user) {
+                  if (!user.activated) {
+                      objects.push('NOT ACTIVATED');
+                      return objects;
+                  }
       try {
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
         token = jwtProvider.createToken(username, user.roles);
@@ -101,27 +125,27 @@ const UserService = {
       }
     }
     return objects;
-  },
-
-  async signup(userDTO) {
-    if (await userRepository.findByUserName(userDTO.userName)) return null;
-    const role = await roleRepository.findUserRoleByRoleName('ROLE_AUTHOR');
-    const user = await userRepository.save(new User(userDTO.userName,
-      passwordEncoder.encode(userDTO.password),
-      [role],
-      userDTO.email,
-    ));
-    user.password = '';
-    return user;
-  },
-
-  async getAll() {
-    return await userRepository.findAll();
-  },
-
-  async getByUserName(userName) {
-    return await userRepository.findByUserName(userName);
-  },
-};
-
+  }
+  const jwtProvider = require('./jwtProvider');
+  const IPLog = require('./IPLog');
+  const ActivityType = require('./ActivityType');
+  const ipLogRepository = require('./ipLogRepository');
+  const requestService = require('./requestService');
+  
+  exports.authenticate = function(username, password, httpServletRequest) {
+    let objects = [];
+    authenticationManager.authenticate(username, password, function(err, user) {
+      if (err) {
+        // handle error
+      } else {
+        let token = jwtProvider.createToken(username, user.roles);
+        objects.push(token);
+        objects.push(user.roles);
+        let log = new IPLog(user, ActivityType.LOGIN);
+        ipLogRepository.save(log);
+        console.log(`user: ${username}, have Email: ${user.email}, ip: ${requestService.getClientIP(httpServletRequest)}`);
+      }
+    });
+    return objects;
+  };
 module.exports = UserService;
